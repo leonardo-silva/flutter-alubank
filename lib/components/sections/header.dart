9e8edd64451729838d8f425dbf08784b7f95bad4
Leonardo Silva <leonardosilvaif@hotmail.com>
2026-01-31 14:53:35 -0300

    feat: styling Header widgets

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -5,15 +5,24 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[Text('\$1000.00'), Text('Balanço disponível')],
-        ),
-        Icon(Icons.account_circle),
-      ],
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                '\$1000.00',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              Text('Balanço disponível', style: TextStyle(fontSize: 16)),
+            ],
+          ),
+          Icon(Icons.account_circle, size: 42),
+        ],
+      ),
     );
   }
 }
