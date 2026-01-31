a4a678d91196ecd733e2e9f72ab0c0ff792c7b9a
Leonardo Silva <leonardosilvaif@hotmail.com>
2026-01-31 14:44:16 -0300

    feat: defining mainAxisAlignment and crossAxisAlignment for Header containers

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -6,8 +6,10 @@ class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[Text('\$1000.00'), Text('Balanço disponível')],
         ),
         Icon(Icons.account_circle),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,6 +6,6 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Header());
+    return const Scaffold(body: Column(children: <Widget>[Header()]));
   }
 }
