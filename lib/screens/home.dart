fc4399e597ddbbb1d314e75b2ba8373db15abe2d
Leonardo Silva <leonardosilvaif@hotmail.com>
2026-02-01 11:16:12 -0300

    feat: new BoxCard component that receives its content dynamically

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:alubank/components/box_card.dart';
 import 'package:alubank/components/sections/header.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +7,13 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Column(children: <Widget>[Header()]));
+    return const Scaffold(
+      body: Column(
+        children: <Widget>[
+          Header(),
+          BoxCard(boxContent: Text('Hello World!')),
+        ],
+      ),
+    );
   }
 }
