e38f2b00f0b48e2f3b0950ceed0d8f342f09d1d0
Leonardo Silva <leonardosilvaif@hotmail.com>
2026-02-03 14:27:25 -0300

    feat: creating _RecentActivityContent for Section RecentActivity

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:alubank/components/box_card.dart';
 import 'package:alubank/components/color_dot.dart';
 import 'package:alubank/components/content_division.dart';
 import 'package:alubank/components/sections/header.dart';
+import 'package:alubank/components/sections/recent_activity.dart';
 import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
 
@@ -11,12 +12,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Header(),
-          BoxCard(boxContent: ContentDivision()),
-        ],
-      ),
+      body: Column(children: <Widget>[Header(), RecentActivity()]),
     );
   }
 }
